a1df4994407615dc119b7329a2643ff57bd3c8e6
Girookerz <tirismaker@gmail.com>
2022-10-16 10:27:31 +0700

    victor-md

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,23 +2,23 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow*
+let str = `*https://www.youtube.com/channel*
 
 Pengen mentahan nya ?
 Nih ada santai bro....
-https://github.com/ImYanXiao/Elaina-MultiDevice
+https://github.com/Victorbotzzz/vctry
 
 Pengen yang udah di recode oleh owner?
-Script bot ini dijual bila kalian mau bisa chat owner
+Chat
 
-wa.me/6282127487538`
+wa.me/6289637582564`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://www.instagram.com/giroo_ktx",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    title: 'Victor-Botz MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
